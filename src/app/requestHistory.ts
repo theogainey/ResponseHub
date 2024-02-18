d7debded2bb0843b82cb215e733d8e2f76cb7687
Theo Gainey <theogainey@gmail.com>
2024-02-18 12:09:11 -0500

    feat: delete single history items + styling

--- src/app/requestHistory.ts
+++ src/app/requestHistory.ts
@@ -124,7 +124,7 @@ class RequestHistoryDB {
     });
   }
 
-  async getDeleteAllRequests():Promise<void> {
+  async deleteAllRequests():Promise<void> {
     await this.initPromise;
     return new Promise((resolve, reject) => {
       if (!this.db) {
@@ -155,13 +155,19 @@ class RequestHistoryDB {
 
       const transaction = this.db.transaction(['requests'], 'readwrite');
       const objectStore = transaction.objectStore('requests');
-      const deleteRequest = objectStore.delete(id);
+      const index = objectStore.index('id');
 
-      deleteRequest.onsuccess = (_event) => {
-        resolve();
+      const getKeyRequest = index.getKey(id);
+
+      getKeyRequest.onsuccess = (event) => {
+        const result = (event.target as IDBRequest).result;
+        const deleteRequest = objectStore.delete(result);
+        deleteRequest.onsuccess = (_event) => {
+          resolve();
+        }
       };
 
-      deleteRequest.onerror = (_event) => {
+      getKeyRequest.onerror = (_event) => {
         reject();
       };
     });
@@ -171,7 +177,6 @@ class RequestHistoryDB {
 
 const historyEntryClickHandler = (id: number) => async ()=> {
   const request = await requestHistoryDB.getRequestById(id);
-  console.log(request);
   setURL(request?.url ?? '');
   setMethod(request?.method ?? 'GET');
   setHeaders(request?.headers ?? []);
@@ -179,12 +184,37 @@ const historyEntryClickHandler = (id: number) => async ()=> {
   printPreview();
 } 
 
+const historyEntryDeleteButtonHandler = (id: number) => async (event:Event) => {
+  event.stopPropagation();
+  await requestHistoryDB.deleteRequestById(id);
+  const parentHistoryEntry = (event.target as Element).closest('.cmp-history__list-item');
+  parentHistoryEntry?.remove();
+};
+
+const createDeleteHistoryEntryButton = (request: RequestHistoryEntry) => {
+  const newDeleteHistoryEntryButton = document.createElement('button');
+  newDeleteHistoryEntryButton.classList.add('cmp-history__delete-entry-button');
+  newDeleteHistoryEntryButton.setAttribute('data-request-id', request.id?.toString() ?? '0');
+  newDeleteHistoryEntryButton.innerHTML = `
+    <svg viewBox="0 0 24 24" fill="none" class="cmp-history__delete-entry-button-svg">
+      <path d="M3 6H21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M19 6V20C19 21 18 22 17 22H7C6 22 5 21 5 20V6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M8 6V4C8 3 9 2 10 2H14C15 2 16 3 16 4V6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M10 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M14 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+  `;
+  newDeleteHistoryEntryButton.addEventListener('click', historyEntryDeleteButtonHandler(request.id ?? 0));
+  return newDeleteHistoryEntryButton;
+}
+
 const createHistoryEntryElement = (request: RequestHistoryEntry) => {
   const newHistoryEntryElement = document.createElement('div');
   newHistoryEntryElement.classList.add('cmp-history__list-item');
   newHistoryEntryElement.setAttribute('data-request-id', request.id?.toString() ?? '0');
   newHistoryEntryElement.innerHTML = `<span class="cmp-history__method cmp-history__method--${request.method}">${request.method}</span> <span>${request.url}</span>`;
   newHistoryEntryElement.addEventListener('click', historyEntryClickHandler(request.id ?? 0));
+  newHistoryEntryElement.append(createDeleteHistoryEntryButton(request));
   return newHistoryEntryElement;
 }
 
@@ -204,7 +234,6 @@ const requestHistoryDB = new RequestHistoryDB();
 
 const initHistory = async () => {
   const requests = await requestHistoryDB.getRequests();
-  console.log(requests);
   printHistory(requests);
   const historyOptionsButton = document.querySelector('.cmp-history__options-button');
   historyOptionsButton?.addEventListener('click', historyOptionsHandler);
@@ -220,9 +249,8 @@ const addRequestToHistory = async (request: RequestHistoryEntry) => {
   printNewHistoryEntry(request);
 };
 
-// TODO: An are you sure workflow
 const clearRequestHistory = async () => {
-  await requestHistoryDB.getDeleteAllRequests();
+  await requestHistoryDB.deleteAllRequests();
   const historyContainer = document.querySelector('.cmp-history__entry-area') as HTMLElement;
   historyContainer.innerHTML = '';
   historyOptionsHandler();
@@ -232,6 +260,7 @@ const clearRequestHistory = async () => {
 const historyOptionsHandler = () => {
   const historyOptionsModal = document.querySelector('.cmp-history__options-modal');
   const historyOptionsButton = document.querySelector('.cmp-history__options-button') as Element;
+  historyOptionsButton.classList.toggle('cmp-history__options-button--active');
   if(historyOptionsModal?.classList.contains('util-display-none')){
     historyOptionsButton.innerHTML = `
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
